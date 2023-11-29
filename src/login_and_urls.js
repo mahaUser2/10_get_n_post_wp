74502387674e611b5cd8344419ae89eee2f8e8a5
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-11-29 09:18:23 +0100

    Delete src/login_and_urls.js

--- src/login_and_urls.js
+++
@@ -1,5 +0,0 @@
-export const baseEndpoint = "https://wp.mfw-ucn.dk/wp-json";
-export const postsUrl = "/wp/v2/posts";
-export const verifyUserEndpoint = "/jwt-auth/v1/token";
-export const username = "api-user@gmail.com";
-export const password = "WRJi rFet uq4t zKTY t3NJ Ni8G";
